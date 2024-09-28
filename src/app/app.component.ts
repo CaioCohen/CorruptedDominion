e15f1c2333d112533b45048fd27649bc24896090
Caio Cohen <caio.cohen@vh.com.br>
2024-09-28 11:53:21 -0300

    correcoes de layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'corruptedDominion';
+  campanha: string | null= "";
+  public constructor(private renderer: Renderer2) {
+
+  }
 
   ngOnInit(): void {
-    if(!('campanha' in sessionStorage)){
+    if (!('campanha' in sessionStorage)) {
       sessionStorage.setItem("campanha", "HW");
+    } else {
+      this.campanha = sessionStorage.getItem("campanha");
     }
+    this.setBackground();
+  }
+
+  setBackground() {
+
+    this.renderer.setStyle(document.body, 'background-image', this.campanha == "HW" ? 'url(assets/imgs/fundoHW.jfif)' : "url(assets/imgs/fundo.jpg)");
+    //this.renderer.setStyle(document.body, 'background-image', "url(assets/imgs/fundoHW.jfif)");
+    this.renderer.setStyle(document.body, 'background-size', '100% 100%'); // Optional: ensure it covers the whole screen
   }
 }
